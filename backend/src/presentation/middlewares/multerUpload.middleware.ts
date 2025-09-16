f1459c520817eb570f062ca5b9a5b3cfcad20fcd
Your Name <mhdameent2006@gmail.com>
2025-09-17 00:52:22 +0530

    fix: mutler middle reate auto uploads folder

--- backend/src/presentation/middlewares/multerUpload.middleware.ts
+++ backend/src/presentation/middlewares/multerUpload.middleware.ts
@@ -1,24 +1,35 @@
-import multer from 'multer';
-import { ErrorMsg } from '../../utils/constants/commonErrorMsg.constants';
-import { CustomError } from '../../utils/errors/custom.error';
-import { HttpResCode } from '../../utils/constants/httpResponseCode.utils';
+import fs from "fs"
+import path from "path"
+import multer from "multer"
+import { ErrorMsg } from "../../utils/constants/commonErrorMsg.constants"
+import { CustomError } from "../../utils/errors/custom.error"
+import { HttpResCode } from "../../utils/constants/httpResponseCode.utils"
+
+const uploadDir = path.resolve("src/uploads")
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'src/uploads/'),
-  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
-});
+  destination: (req, file, cb) => {
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true })
+    }
+    cb(null, uploadDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  },
+})
 
 export const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true)
     } else {
-      cb(new CustomError(ErrorMsg.ALLOWED_FILE_TYPE, HttpResCode.BAD_REQUEST));
+      cb(new CustomError(ErrorMsg.ALLOWED_FILE_TYPE, HttpResCode.BAD_REQUEST))
     }
-  }
-});
+  },
+})
 
 export const imageUpload = upload.fields([
   { name: "files", maxCount: 10 },
